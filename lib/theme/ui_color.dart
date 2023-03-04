385c460fd1f14d6135c75bd79ef179bd884b728e
Charles Santos <csbs.conta@outlook.com>
2023-03-04 08:34:22 -0300

    primeiro comite

---
+++ lib/theme/ui_color.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/animation.dart';
+
+class UiColor {
+  static const Color task = Color(0xFFc89550);
+  static const Color calendar = Color(0xFFfcd447);
+  static const Color completed = Color(0xFFd75a31);
+  static const Color list = Color(0xFF698ee5);
+  static const Color planning = Color(0xFF54c893);
+  static const Color element = Color(0xFF3f3f3f);
+  static const Color back = Color(0xFF1b1c1b);
+  static const Color text_1 = Color(0xFFfefefe);
+  static const Color text_2 = Color(0xFFbcbcbc);
+}
